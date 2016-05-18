a98c06adb74b3aa58198dbf8e0ed8d8539a1841b
Ricardo Simão <rpsimao@gmail.com>
2016-05-18 16:32:17 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,100 @@
+
+<!doctype html>
+
+<html lang="en" class="no-js">
+
+<head>
+
+    <!-- meta data -->
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+
+    <!-- title and favicon -->
+
+    <title>SB Custom :: Boutique Guitar and Bass Pickups - Custom Guitars and Basses</title>
+    <link rel="icon" href="assets/img/icon/fav_icon.gif">
+    
+
+    <!--necessary stylesheets -->
+
+    <link type="text/css" rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/ionicons.min.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/popup.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/owl.carousel.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/owl.theme.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/style.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/sbcustom.css">
+
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+    <!--[if IE]>
+		<style>
+		    .flip-container:hover .back,
+			.flip-container.hover .back {
+			    backface-visibility: visible !important;
+			}
+		</style>
+	<![endif]-->
+
+
+</head>
+
+
+<body>
+    
+    <!-- Preloader -->
+
+    <div id="preloader">
+        <div class="loader">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+
+    <!-- home-page -->
+    
+    <?php include_once ("partials/home.phtml"); ?>
+    
+
+    
+    <div class="close-btn"></div>
+    
+
+
+    <?php include_once ("partials/guitarpickups.phtml") ?>
+
+
+    <?php include_once ("partials/basspickups.phtml") ?>
+
+
+
+    <?php include_once ("partials/custom.phtml") ?>
+    
+
+
+    <?php include_once ("partials/work.phtml") ?>
+    
+
+    
+    <script type="text/javascript" src="assets/js/jquery-2.1.3.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/modernizr.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.easing.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.mixitup.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.popup.min.js"></script>
+    <script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script type="text/javascript" src="assets/js/contact.js"></script>
+    <script type="text/javascript" src="assets/js/script.js"></script>
+    <script type="text/javascript" src="assets/js/sbcustom.js"></script>
+
+</body>
+</html>
