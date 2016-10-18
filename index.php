c9bb57dba0636e4a5c230c8a8918b6ffa9d91158
rpsimao <ricardo.simao@upgrade.pt>
2016-10-18 19:38:44 +0100

    Mods

--- index.php
+++ index.php
@@ -1,24 +1,14 @@
-
 <!doctype html>
-
 <html lang="en" class="no-js">
-
 <head>
-
     <!-- meta data -->
-
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name=viewport content="width=device-width, initial-scale=1">
-
     <!-- title and favicon -->
-
     <title>SB Custom :: Boutique Guitar and Bass Pickups - Custom Guitars and Basses</title>
     <link rel="icon" href="assets/img/icon/fav_icon.gif">
-
-
     <!--necessary stylesheets -->
-
     <link type="text/css" rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link type="text/css" rel="stylesheet" href="assets/css/font-awesome.min.css">
     <link type="text/css" rel="stylesheet" href="assets/css/ionicons.min.css">
@@ -28,12 +18,10 @@
     <link type="text/css" rel="stylesheet" href="assets/css/style.css">
     <link type="text/css" rel="stylesheet" href="assets/css/sbcustom.css">
     <link type="text/css" rel="stylesheet" href="assets/css/pickups.css">
-
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-
     <!--[if IE]>
 		<style>
 		    .flip-container:hover .back,
@@ -45,14 +33,9 @@
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-85357785-1', 'auto');ga('send', 'pageview');
     </script>
-
 </head>
-
-
 <body>
-
     <!-- Preloader -->
-
     <div id="preloader">
         <div class="loader">
             <span></span>
@@ -61,7 +44,6 @@
             <span></span>
         </div>
     </div>
-
     <!-- home-page -->
     <?php include_once ("partials/home.phtml"); ?>
     <div class="close-btn"></div>
@@ -80,6 +62,5 @@
     <script type="text/javascript" src="assets/js/contact.js"></script>
     <script type="text/javascript" src="assets/js/script.js"></script>
     <script type="text/javascript" src="assets/js/sbcustom.js"></script>
-
 </body>
 </html>
