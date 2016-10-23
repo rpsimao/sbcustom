6701dfd3f2d0b878348f77d75593bfb46864f6b3
rpsimao <ricardo.simao@upgrade.pt>
2016-10-23 21:17:58 +0100

    Add Support

--- Library/CommonStrings.php
+++ Library/CommonStrings.php
@@ -5,24 +5,17 @@
  * rpsimao
  * 20/10/16
  */
-
-
 namespace SBCustom;
-
 
 class CommonStrings
 {
 
     public static function text($what)
     {
-
         $strings = [
-            'pickups_description_box' => 'Like all SB Custom pickups these are shipped in a premium product box, and come with all mounting hardware and a wiring diagram.',
+            'pickups_description_box' => 'Like all SB Custom pickups these are shipped in a premium product box, and come with all mounting hardware, a wiring diagram and a Certificate of Quality and Authenticity.',
 
         ];
-
         return $strings[$what];
-
     }
-
 }
